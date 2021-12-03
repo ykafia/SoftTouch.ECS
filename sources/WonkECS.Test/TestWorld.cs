2420ec187790d16c781878df44977d1dd5d8c524
Youness KAFIA <kafia.youness@hotmail.fr>
2021-12-03 11:49:40 +0100

    added tests

--- sources/WonkECS.Test/TestWorld.cs
+++ sources/WonkECS.Test/TestWorld.cs
@@ -7,6 +7,19 @@ namespace WonkECS.Test
     [TestClass]
     public class UnitTest1
     {
+
+        [TestMethod]
+        public void TestAddComponent()
+        {
+            var world = new World();
+            world
+                .CreateEntity()
+                .With(new NameComponent{Name = "Lola"})
+                .Build();
+            Assert.IsTrue(world[0].Has<NameComponent>());
+            Assert.AreEqual(world[0].Get<NameComponent>().Name,  "Lola");
+        }
+
         [TestMethod]
         public void TestAddSameComponent()
         {
@@ -17,7 +30,21 @@ namespace WonkECS.Test
                 .With(new NameComponent{Name = "Jojo"})
                 .Build();
             
-            world[0].
+            Assert.AreEqual(world[0].Get<NameComponent>().Name,  "Jojo");
+        }
+
+        [TestMethod]
+        public void TestRemoveComponent()
+        {
+            var world = new World();
+            world
+                .CreateEntity()
+                .With(new NameComponent{Name = "Lola"})
+                .With(new HealthComponent{LifePoints = 100})
+                .Build();
+            world[0].Remove<NameComponent>();
+            Assert.IsFalse(world[0].Has<NameComponent>());
+            Assert.IsTrue(world[0].Has<NameComponent>());
         }
     }
 }
