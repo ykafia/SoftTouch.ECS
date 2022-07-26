fe7f3bedaeba36c24bf1947deaced8847e732f5c
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2022-07-26 17:44:25 +0200

    update with F# api

---
+++ sources/ECSharp.FSharp.Example/Program.fs
@@ -0,0 +1,22 @@
+﻿open ECSharp.FSharp
+open ECSharp
+open System.Runtime.CompilerServices
+
+// For more information see https://aka.ms/fsharp-console-apps
+
+type NameComponent = 
+    struct
+        val Name : string
+        new (n : string) = {Name = n}
+    end
+
+let w = new World()
+
+let martha = NameComponent("Martha")
+
+w 
+|> World.CreateEntity
+|> Entity.WithValue<NameComponent> (NameComponent "Martha")
+|> ignore
+
+printfn "Hello %s" (w |> World.GetEntity 0 |> Entity.Get<NameComponent>).Name
---
+++ sources/ECSharp.FSharp/Library.fs
@@ -0,0 +1,21 @@
+﻿namespace ECSharp.FSharp
+
+open ECSharp
+
+module World =
+    let CreateEntity (world : World) =
+        world.CreateEntity()
+    let GetEntity  (index : int64) (world : World)=
+        world[index]
+
+module Entity =
+    let With<'T when 'T : struct and 'T : (new: unit -> 'T) and 'T :> System.ValueType> (builder : EntityBuilder) =
+        builder.With<'T>()
+    
+    let WithValue<'T when 'T : struct and 'T : (new: unit -> 'T) and 'T :> System.ValueType> (c : 'T) (builder : EntityBuilder) =
+        builder.With(&c)
+    
+    let Get<'T when 'T : struct and 'T : (new: unit -> 'T) and 'T :> System.ValueType> (arch : ArchetypeRecord) =
+        arch.Get<'T>()
+    let Has<'T when 'T : struct and 'T : (new: unit -> 'T) and 'T :> System.ValueType> (arch : ArchetypeRecord) =
+        arch.Get<'T>()
